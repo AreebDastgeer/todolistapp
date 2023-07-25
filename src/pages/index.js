c153c45e18098f3eb333185f87aa96338da9d01c
AreebDastgeer <dastgeerareeb@gmail.com>
2023-07-26 01:29:51 +0500

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import React  from "react";
-import Link from 'next/link';
-import  Homepage  from './homepage';
+import SignUp from './signup';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Index() {
   return (<main>
-    <Link href="/signup">Sign Up</Link>
-   <Homepage/>
+   <SignUp />
 </main>
   )
 }
