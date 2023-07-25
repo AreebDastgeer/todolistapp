e6aecf50ed2307b085b41f26625ac3a1fa322284
AreebDastgeer <dastgeerareeb@gmail.com>
2023-07-26 01:24:40 +0500

    signup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import React  from "react";
+import Link from 'next/link';
 import  Homepage  from './homepage';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Index() {
-  return (
+  return (<main>
+    <Link href="/signup">Sign Up</Link>
    <Homepage/>
-
+</main>
   )
 }
